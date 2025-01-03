62c03caf14f4963b7cc6790ba88708646e66f9d1
Lavanya572 <lavan572cp@gmail.com>
2025-01-03 21:12:25 +0530

    Added new feature to engage user, but still getting rejected

--- cplusplus.cpp
+++ cplusplus.cpp
@@ -1,9 +1,21 @@
+// Till user says please the loop goes on, still gets rejected
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 int main() {
     std::string input;
-    std::getline(std::cin, input);
-    std::cout << "I'm sorry, I can't assist with it." << std::endl;
+    while (true) {
+        std::cout << "Say something: ";
+        std::getline(std::cin, input);
+        std::string inputLower = input;
+        std::transform(inputLower.begin(), inputLower.end(), inputLower.begin(), ::tolower);
+        if (input.find("please") != std::string::npos) {
+            std::cout << "Thank you for being polite. Still I can't do it." << std::endl;
+            break;
+        } else {
+            std::cout << "I'm sorry, I can't assist with it. You weren't polite. Try again." << std::endl;
+        }
+    }
     return 0;
 }
