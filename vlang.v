6d6fc1c8e79ec602e568f6134b402b6ee6b6a189
Dharshan2208 <dharshan080622@gmail.com>
2025-07-15 23:10:00 +0530

    done proper error handling in vlang.v

--- vlang.v
+++ vlang.v
@@ -2,6 +2,9 @@ import readline
 
 fn main() {
     mut r := readline.Readline{}
-    answer := r.read_line('Ask me something: ')!
+    answer := r.read_line('Ask me something: ') or {
+            println('Failed to read input: $err')
+            return
+        }
     println('Sorry, I can\'t help you with this. $answer')
 }
